56b5dbc64c41e16ba4655494edb0551de5d21bd2
Brian Lesko <brianjosephlesko@gmail.com>
2023-11-17 18:10:31 -0500

    gui update

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from bs4 import BeautifulSoup
 import streamlit as st
 import requests
 import bs4 as bs4
-from gui_cusomizations import about
-from gui_cusomizations import hide_streamlit_header_footer
 import numpy as np
+from customize_gui import gui
+gui = gui()
 
 def scrape_text_from(url):
     try:
@@ -34,49 +34,35 @@ def get_links_from(url):
     return links
 
 def main():
-    hide_streamlit_header_footer()
-
-    with st.sidebar:
-        about()
-
+    gui.about(text = "In this code you can scrape the text and links from any website.")
+    gui.clean_format()
     st.title('Web Scraper')
-    #url = st.text_input("Paste a URL here")
-
     url = st.chat_input("Paste a URL here")
-    
     if url: 
         st.chat_message("user").write(url)
-
         links = get_links_from(url)
         text = scrape_text_from(url)
-
-        response_1 = "This Website containts " + str(len(text)) + " characters and " + str(len(links)) + " links"
-
-        st.chat_message("assistant").write(response_1)
-
-        response_2 = 'The first words are: " ' + text[:50] + ' ..." '
-
-        st.chat_message("assistant").write(response_2)
-
-        response_3 = 'Would you like to download the text or links?'
-
-        st.chat_message("assistant").write(response_3)
+        with st.chat_message("assistant"): 
+            response_1 = "This Website containts " + str(len(text)) + " characters and " + str(len(links)) + " links"
+            st.write(response_1)
+            response_2 = 'The first words are: " ' + text[:50] + ' ..." '
+            st.write(response_2)
+            response_3 = 'Would you like to download the text or links?'
+            st.write(response_3)
 
         col1,  col2, gap, col3, col4 = st.columns([.5,1,.5,1,.5])
         with col2:
-            text_file = text
-            file_name = "text fronm" + url + ".txt"
             st.download_button(
-                label="Download Text as .txt",data=text_file,
-                file_name=file_name,
+                label="Download Text as .txt",
+                data=text,
+                file_name="text fronm" + url + ".txt",
                 mime="text/plain"
                 )
         with col3:
-            csv_links = '\n'.join(np.array(links).flatten())
-            file_name = "links_from_" + url.replace("://", "_").replace("/", "_") + ".txt"
             st.download_button(
-                label="Download Links as .txt",data=csv_links,
-                file_name=file_name,
+                label="Download Links as .txt",
+                data='\n'.join(np.array(links).flatten()),
+                file_name="links_from_" + url.replace("://", "_").replace("/", "_") + ".txt",
                 mime="text/plain"
                 )
 main()
